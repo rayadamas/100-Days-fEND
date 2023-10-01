2e65db513f6abcac2c1a868454955ff89cc5da38
Ray Adamas <djadamasray@gmail.com>
2023-10-01 17:10:03 -0500

    quiz 1

--- practice.js
+++ practice.js
@@ -1,4 +1,10 @@
-for (var i = 0; i < 10; i++) {
-    console.log(i)
-}
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
 
+// Quiz: First Expression
+// write an expression that uses at least 3 different arithmetic operators
+// whose result evaluates to 42
+console.log(31 + 13 - 2);
+console.log(210 / 5);
+console.log(142 % 5 + 40);
