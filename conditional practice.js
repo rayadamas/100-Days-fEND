19c935d7f1f62a03c4dcb4d606aeb7c48dfaf7fc
Ray Adamas <djadamasray@gmail.com>
2023-10-02 12:21:33 -0500

    musical groups quiz. greater than quarter doesn't need to be 5

--- conditional practice.js
+++ conditional practice.js
@@ -23,3 +23,34 @@ if (number % 2 === 0) {
 } else {
     console.log("odd");
 }
+
+// Quiz: Musical Groups
+// Write a series of conditional statements that:
+// prints "not a group" if musicians is less than or equal to 0
+// prints "solo" if musicians is equal to 1
+// prints "duet" if musicians is equal to 2
+// prints "trio" if musicians is equal to 3
+// prints "quartet" if musicians is equal to 4
+// prints "this is a large group" if musicians is greater than 4
+// TIP: Make sure to test your code with different values. For example,
+// If musicians equals 3, then trio should be printed to the console.
+// If musicians equals 20, then this is a large group should be printed to the console.
+// If musicians equals -1, then not a group should be printed to the console.
+// change the value of `musicians` to test your conditional statements
+var musicians = 5;
+if (musicians <= 0) {
+ console.log("not a group");
+} else if (musicians === 1) {
+ console.log("solo");
+} else if (musicians === 2) {
+ console.log("duet");
+}
+else if (musicians === 3) {
+ console.log("trio");
+}
+else if (musicians === 4) {
+ console.log("quartet");
+}
+else if (musicians > 4) {
+ console.log("this is a large group");
+}
