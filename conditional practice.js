5f3759631c94e20f11daf6bc319ff3731c48bd6f
Ray Adamas <djadamasray@gmail.com>
2023-10-02 13:13:59 -0500

    whom done it completed

--- conditional practice.js
+++ conditional practice.js
@@ -96,3 +96,26 @@ Afterwards, use this template to print a message to the console if the mystery w
 
 */
 
+var room = "dining room";
+var suspect = "Mr. Kalehoff";
+
+var weapon = "";
+var solved = false;
+
+if (room === "ballroom" && suspect === "Mr. Kalehoff") {
+    weapon = "poison";
+    solved = true;
+} else if (room === "billiards room" && suspect === "Mrs. Sparr") {
+    weapon = "pool stick";
+    solved = true;
+} else if (room === "gallery" && suspect === "Ms. Van Cleve") {
+    weapon = "trophy";
+    solved = true;
+} else if (room === "dining room" && suspect === "Mr. Parkes") {
+    weapon = "knife";
+    solved = true;
+}
+
+if (solved) {
+    console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
