6e7ede7176dd79874d0a062a10e045ca7f9b2c47
Ray Adamas <djadamasray@gmail.com>
2023-10-02 12:56:14 -0500

    murder mystery instructions

--- conditional practice.js
+++ conditional practice.js
@@ -62,3 +62,37 @@ else if (musicians > 4) {
 // (!false) // true
 // true && true && true // true
 //
+
+// Murder Mystery
+/*
+Here's what we know! In this murder mystery there are:
+
+- **four rooms**: the ballroom, gallery, billiards room, and dining room,
+- **four weapons**: poison, a trophy, a pool stick, and a knife,
+- and **four suspects**: Mr. Parkes, Ms. Van Cleve, Mrs. Sparr, and Mr. Kalehoff.
+
+We also know that each weapon _corresponds_ to a particular room, so...
+
+- the `poison` belongs to the `ballroom`,
+- the `trophy` belongs to the `gallery`,
+- the `pool stick` belongs to the `billiards room`,
+- and the `knife` belongs to the `dining room`.
+
+And we know that each suspect was located in a specific room at the time of the murder.
+
+- `Mr. Parkes` was located in the `dining room`.
+- `Ms. Van Cleve` was located in the `gallery`.
+- `Mrs. Sparr` was located in the `billiards room`.
+- `Mr. Kalehoff` was located in the `ballroom`.
+
+To help solve this mystery, write a combination of conditional statements that:
+
+1. sets the value of `weapon` based on the `room` and
+2. sets the value of `solved` to `true` if the value of `room` matches the `suspect`'s room
+
+Afterwards, use this template to print a message to the console if the mystery was solved:
+
+`__________ did it in the __________ with the __________!`
+
+*/
+
