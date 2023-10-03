bc700c993287fef0084779c7a12af4197bfbe930
Ray Adamas <djadamasray@gmail.com>
2023-10-03 14:29:37 -0500

    clothes size instructions

--- conditional practice.js
+++ conditional practice.js
@@ -195,3 +195,64 @@ if (flavor === "vanilla" || flavor === "chocolate") {
     }
   }
 }
+
+// Quiz: What do I Wear?
+
+/*
+|**Size**|**Width**|**Length**|**Sleeve**|
+|---|---|---|---|
+|S|18"|28"|8.13"|
+|M|20"|29"|8.38"|
+|L|22"|30"|8.63"|
+|XL|24"|31"|8.88"|
+|2XL|26"|33"|9.63"|
+|3XL|28"|34"|10.13"|
+
+Use the sizing chart above, create a series of logical expressions that prints the **size** of a t-shirt based on the measurements of
+`shirtWidth`, `shirtLength`, and `shirtSleeve`. Valid sizes include `S`, `M`, `L`, `XL`, `2XL`, and `3XL`.
+
+For example, if...
+
+`var shirtWidth = 23; // size L (large) var shirtLength = 30; // size L (large) var shirtSleeve = 8.71; // size L (large)`
+
+Then print `L` to the console.
+
+**Hint:** _You will need to compare a range of values when checking for `shirtWidth`, `shirtLength`, and `shirtSleeve`.
+For example, if the shirt's **width** is at least **20"**, but no more than **22"**, then the t-shirt should be
+**medium (M)** — as long as the other values for the shirt's **length** and **sleeve** measurements match up._
+
+---
+
+If `shirtWidth`, `shirtLength`, and `shirtSleeve` don't fit within the range of acceptable values for a specific **size**,
+then print `NA` to the console. For example, if...
+
+`var shirtWidth = 18; // size S (small) var shirtLength = 29; // size M (medium) var shirtSleeve = 8.47; // size M (medium)`
+
+Then print `N/A` to the console because the measurements don't all match up with one particular `size`.
+
+> **TIP:** Make sure to test your code with different values. For example,  
+>   
+> If `shirtWidth` equals `19`, `shirtLength` equals `28` and `shirtSleeve` equals `8.21`, then `S` should be printed to the console.  
+> If `shirtWidth` equals `26`, `shirtLength` equals `33` and `shirtSleeve` equals `9.63`, then `2XL` should be printed to the console.  
+> If `shirtWidth` equals `18`, `shirtLength` equals `29` and `shirtSleeve` equals `8.47`, then `NA` should be printed to the console.
+
+ * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
+ *   - shirtWidth
+ *   - shirtLength
+ *   - shirtSleeve
+ *
+ * Use the chart above to print out one of the following correct values:
+ *   - S, M, L, XL, 2XL, or 3XL
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `shirtWidth`, `shirtLength`, and `shirtSleeve`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should use logical expressions
+ * 4. Your code should produce the expected output
+ * 6. Your code should not be empty
+ * 7. BE CAREFUL ABOUT THE EXACT CHARACTERS TO BE PRINTED
+
+
+*/
