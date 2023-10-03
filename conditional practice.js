d70f51c7e3f7c2a82757ae432894b13853a516fa
Ray Adamas <djadamasray@gmail.com>
2023-10-03 17:25:27 -0500

    BTS quiz done

--- conditional practice.js
+++ conditional practice.js
@@ -308,3 +308,75 @@ var category = eatsAnimals ? (eatsPlants ? "omnivore" : "herbivore") : eatsPlant
 console.log(category);
 
 // Quiz: Back to School
+/*
+Write a switch statement to set the average salary of a person based on their type of completed education.
+
+Afterwards, print the following to the console.
+
+In 2015, a person with __________ earned an average of __________/year.
+Fill in the blanks with the type of education and the expected average salary. Make sure to use correct grammar in your printed statement, and watch out for any extra or missing characters
+(including spaces and punctuation marks).
+For help, refer to the findings above.
+
+In 2015, a person with a Bachelor's degree earned an average of $59,124/year.
+TIP: To print out the average salary with commas (i.e. 59,124), use the toLocaleString() method and pass it the locale "en-US".
+For example, salary.toLocaleString("en-US").
+
+TIP: Make sure to test your code with different values. For example,
+
+If education equals "an Associate's degree", then the string"In 2015, a person with an Associate's degree earned an average of $41,496/year."
+should be printed to the console.
+
+no high school diploma earned an average of $25,636/year,
+a high school diploma earned an average of $35,256/year,
+an Associate's degree earned an average of $41,496/year,
+a Bachelor's degree earned an average of $59,124/year,
+a Master's degree earned an average of $69,732/year,
+a Professional degree earned an average of $89,960/year,
+and a Doctoral degree earned an average of $84,396/year.
+
+ * Write a switch statement to set the average salary of a person based on their type of completed education.
+ *
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `education`, and `salary`
+ * - Your code should include a switch statement
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */ 
+
+// change the value of `education` to test your code
+var education = 'a Doctoral degree';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+switch (education) {
+    case "no high school diploma":
+        salary = 25636;
+        break;
+    case "a high school diploma":
+        salary = 35256;
+        break;
+    case "an Associate's degree":
+        salary = 41496;
+        break;
+    case "a Bachelor's degree":
+        salary = 59124;
+        break;
+    case "a Master's degree":
+        salary = 69732;
+        break;
+    case "a Professional degree":
+        salary = 89960;
+        break;
+    case "a Doctoral degree":
+        salary = 84396;
+        break;
+    }
+
+salary = salary.toLocaleString("en-US");
+console.log("In 2015, a person with " + education + " earned an average of $" + salary + "/year.");
