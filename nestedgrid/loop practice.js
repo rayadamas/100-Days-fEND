030548afa7c548a76b5c27b6eb4628181392b976
Ray Adamas <djadamasray@gmail.com>
2023-10-04 13:10:30 -0500

    quiz complete

--- nestedgrid/loop practice.js
+++ nestedgrid/loop practice.js
@@ -13,3 +13,16 @@
 
 // change the value of `x` to test your code
 var x = 1;
+
+while (x <= 20) {
+ if (x % 3 === 0 && x % 5 === 0) {
+  console.log("JuliaJames");
+ } else if (x % 3 === 0) {
+  console.log("James");
+ } else if (x % 5 === 0) {
+  console.log("Julia");
+ } else {
+  console.log(x);
+ }
+ x++;
+}
