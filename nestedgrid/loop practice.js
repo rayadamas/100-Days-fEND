6160ebab66fdab2f87a3216318542e3527835e68
Ray Adamas <djadamasray@gmail.com>
2023-10-04 17:40:37 -0500

    changing while loop to for loop

--- nestedgrid/loop practice.js
+++ nestedgrid/loop practice.js
@@ -127,3 +127,27 @@ while (sec >= 0) {
   }
   sec--;
 }
+
+// Rewrite the following while loop as a for loop:
+
+var x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+}
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `9`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should decrement `x` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+// change the value of `x` to test your code
+for (var x = 9; x >= 1; x--) {
+  console.log("hello " + x);
+}
