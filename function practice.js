5bb71049dc4decb326afcdff01b98d0f7d0a0ec4
Ray Adamas <djadamasray@gmail.com>
2023-10-06 15:21:48 -0500

    func complete

---
+++ function practice.js
@@ -0,0 +1,16 @@
+// Laught it off
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()` function
+ inside `console.log()`
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+ function laugh() {
+  var output = "hahahahahahahahahaha!";
+  return output;
+ }
+
+ console.log(laugh());
