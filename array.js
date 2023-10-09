138cb608717a5fdbd886f2d401d58bc601b0b3ba
Ray Adamas <djadamasray@gmail.com>
2023-10-08 20:39:40 -0500

    zero indexorrrr

--- array.js
+++ array.js
@@ -1,1 +1,16 @@
 // Arrays
+var donuts = ["glazed", "sprinkled", "chocolate"]; // this is an array using 3 string elements
+
+var mixedData = ["qwerty", 123, true, "hello"]; // this is an array using 4 different types of data
+
+var arraysInArrays = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]]; // this is an array in an array
+
+var arraysInArraysInArrays = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+]; // this is an array in an array in an array
+
+console.log(donuts[2]); // prints out "chocolate" to the console
