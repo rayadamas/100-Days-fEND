3fc652115a584aa6bb8f42da0cfed80d19a01740
Ray Adamas <djadamasray@gmail.com>
2023-10-09 12:50:08 -0500

    array property quiz

--- array.js
+++ array.js
@@ -122,3 +122,28 @@ var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
 rainbow.splice(2, 1, 'Yellow', 'Green');
 rainbow.splice(4, 0, 'Purple');
 console.log(rainbow); // prints out the array to the console
+
+// Quidditch Cup
+/*
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+Create a function called hasEnoughPlayers() that takes the team array as an argument and
+returns true or false depending on if the array has at least seven players.
+* QUIZ REQUIREMENTS
+ * - Your code should have a function `hasEnoughPlayers()`
+ * - Your function `hasEnoughPlayers()` should accept one parameter
+ * - Your function `hasEnoughPlayers()` should return the expected output - a Boolean value - true or false
+ * - Return true if the array size is atleast 7, otherwise false. 
+ *
+ */
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+
+function hasEnoughPlayers(team) {
+  if (team.length >= 7) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(hasEnoughPlayers(team)); // prints out true to the console
