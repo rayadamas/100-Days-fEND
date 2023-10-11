273dad166723f9b6ecefc9e34a2f496d5cf0dc0a
Ray Adamas <djadamasray@gmail.com>
2023-10-11 18:10:10 -0500

    targeting size elements

--- pixelart/design.js
+++ pixelart/design.js
@@ -5,6 +5,10 @@ let color = document.getElementById("colorPicker");
 // targets populated color box users are able to click via colorPicker ID in index.html
 
 // Select size input
+let size = document.getElementById("sizePicker"); // ref form containing dimension selectors via sizePicker
+let height = document.getElementById("inputHeight"); // ref height input form via inputHeight
+let width = document.getElementById("inputWidth"); // ref width input form via inputWidth
+let grid = document.getElementById("pixelCanvas"); // ref the space where table is created via pixelCanvas
 
 // When size is submitted by the user, call makeGrid()
 
