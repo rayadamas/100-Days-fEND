e0f23fa124520f0ab5191344a55e55e422958728
Ray Adamas <djadamasray@gmail.com>
2023-10-11 18:09:20 -0500

    target color picker

--- pixelart/design.js
+++ pixelart/design.js
@@ -1,4 +1,8 @@
 // Select color input
+let color = document.getElementById("colorPicker");
+// we target the webpage itself with document
+// getElementById method returns an element w/ a specified value
+// targets populated color box users are able to click via colorPicker ID in index.html
 
 // Select size input
 
