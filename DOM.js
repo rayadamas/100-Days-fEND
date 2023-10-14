a540163ad73a7113f1ce159f2e8d500cab31e2aa
Ray Adamas <djadamasray@gmail.com>
2023-10-13 19:37:34 -0500

    For of quiz

--- DOM.js
+++ DOM.js
@@ -70,3 +70,48 @@ const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 
 const [one,,,two,,,,three] = things;//The empty commas , are used to skip elements in the things array that we don't want to assign to variables.
 const colors = `List of Colors: ${one}, ${two}, ${three}`;
 console.log(colors);
+
+// For in
+for (const thing in things) {
+ console.log(things[thing]);
+}
+
+// For of
+const items = ['item1', 'item2', 'item3'];
+for (const item of items) {
+ console.log(item);
+}
+
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const digit of digits) {
+ if (digit % 2 === 0) {
+     continue;//continue to the next iteration of the loop if the condition is met.
+ }
+ console.log(digit);
+}
+
+// Writing a For...of Loop
+/*
+Write a for...of loop that:
+
+loops through each day in the days array
+capitalizes the first letter of the day
+and prints the day out to the console
+Your code should log the following to the console:
+
+Sunday
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday
+*/
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+for (const day of days) {
+ console.log(day[0].toUpperCase() + day.slice(1));// day[0] is the first letter of the day string.
+ //We use the toUpperCase() method to capitalize it. Then we add it to the rest of the string using the slice() method.
+ //slice(1) returns the rest of the string beginning at the second character.
+}
